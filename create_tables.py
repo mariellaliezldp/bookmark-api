25208cf369904e48c3ecc5a93b85423c2aa19673
mariellaliezldp <mariellaliezld@gmail.com>
2026-06-06 23:30:48 +0800

    feat: setup alembic initial migration

--- create_tables.py
+++
@@ -1,8 +0,0 @@
-from app.database import Base, engine
-import app.models.user
-import app.models.bookmark
-import app.models.tag
-import app.models.bookmark_tags
-
-Base.metadata.create_all(bind=engine)
-print("Tables created successfully")
